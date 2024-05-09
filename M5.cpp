254810901bbac28fff4e37e9f986985355502794
Александр Осипов <alikxyzet@gmail.com>
2024-05-09 15:29:03 +0300

    5.2.2 Example

--- M5.cpp
+++ M5.cpp
@@ -5,19 +5,18 @@ int main()
 {
 	try
 	{
-		throw std::string("example");
+		try
+		{
+			throw - 1;
+		}
+		catch (const std::string& str)
+		{
+			std::cerr << "We caught an exception of type std::string" << '\n';
+		}
 	}
 	catch (int a)
 	{
 		std::cerr << "We caught an int exception with value: " << a << '\n';
-	}
-	catch (double)
-	{
-		std::cerr << "We caught an exception of type double" << '\n';
-	}
-	catch (const std::string& str)
-	{
-		std::cerr << "We caught an exception of type std::string" << '\n';
 	}
 
 	std::cout << "Continuing our way!\n";
