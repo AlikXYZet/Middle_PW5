f9e5c1bb9acb0fd6c3cceaf5c3383390f3bf6f36
Александр Осипов <alikxyzet@gmail.com>
2024-05-09 18:09:17 +0300

    5.7 Example ["Functional try block"]

--- M5.cpp
+++ M5.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
-#include <exception>
-#include <string>
+
+class A
+{
+private:
+	int m_x;
+
+public:
+	A(int x) : m_x(x)
+	{
+		if (x <= 0)
+			throw 1;
+	}
+};
+
+class B : public A
+{
+public:
+	B(int x) try : A(x)
+	{
+	}
+	catch (...)
+	{
+		std::cerr << "Construction of A failed\n";
+	}
+
+};
 
 int main()
 {
 	try
 	{
-		throw std::runtime_error("Bad things happened");
+		B b(0);
 	}
 	catch (std::exception& exception)
 	{
-		std::cerr << "Standart exception: " << exception.what() << '\n';
+		std::cout << "Oops!\n";
 	}
 
 	return 0;
