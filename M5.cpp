e86e757cb073fadc3a7fc3e69da1a939e677813e
Александр Осипов <alikxyzet@gmail.com>
2024-05-10 00:10:06 +0300

    PW5: Code correction [Version 1]

--- M5.cpp
+++ M5.cpp
@@ -10,15 +10,15 @@ public:
 	Fraction(int a, int b)
 		: numerator(a), denominator(b)
 	{
+		// Исключение ввода нуля в качестве делителя
 		if (denominator == 0)
 			throw std::runtime_error("Attempt to divide by zero!");
-		//numerator = a;
-		//denominator = b;
 	}
 
+	// Перегрузка вывода данных
 	friend std::ostream& operator<<(std::ostream& out, const Fraction& f)
 	{
-		out << static_cast<double>(f.numerator) / f.denominator;;
+		out << static_cast<double>(f.numerator) / f.denominator;
 		return out;
 	}
 };
@@ -26,21 +26,38 @@ public:
 int main()
 {
 	int inA, inB;
-	std::cout << "Enter the numerator and denominator numbers." << '\n';
-	std::cout << "Numerator: ";
-	std::cin >> inA;
-	std::cout << "Denominator: ";
-	std::cin >> inB;
-	std::cout << '\n';
+
+	std::cout << "Enter two integers to calculate division." << '\n';
 
 	try
 	{
+		// Переменные проверки вводимых данных
+		char checkA, checkB;
+		// Пропуск "whitespace" (пробелы, табы, символы новой строки)
+		std::cin.unsetf(std::ios_base::skipws);
+
+		// Ввод inA
+		std::cout << "Numerator: ";
+		std::cin >> inA >> checkA;
+
+		// Проверка корректного ввода inA
+		if (checkA != '\n')
+			throw std::exception("Invalid Numerator data");
+
+		// Ввод inB
+		std::cout << "Denominator: ";
+		std::cin >> inB >> checkB;
+
+		// Проверка корректного ввода inB
+		if (checkB != '\n')
+			throw std::exception("Invalid Denominator data");
+
 		Fraction fraction(inA, inB);
-		std::cout << "Calculated fraction: " << fraction << std::endl;
+		std::cout << "\nCalculated fraction: " << fraction << std::endl;
 	}
 	catch (std::exception& exception)
 	{
-		std::cerr << "Error: " << exception.what() << '\n';
+		std::cerr << "\nError: " << exception.what() << '\n';
 	}
 
 	return 0;
